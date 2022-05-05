2b11f734ccff6d93529c78388de55db3ee79123a
Michael Balsamo <mjb10@me.com>
2022-05-05 10:37:56 -0500

    Cleaned up code for readability

--- Program.cs
+++ Program.cs
@@ -4,67 +4,59 @@ namespace dining_dollars
 {
     class Program
     {
-
-        // void dining() {
-
-            
-        // }
-
         static void Main(string[] args)
         {
+            const int SEMESTER_END = 124; // May 4th is the last day of finals, and the 124th day of the year
 
-            const int SEMESTEREND = 124; //may 4th is the 94th day of the year, and last day of finals
-
-            Console.WriteLine("Type one of the following: \n  Dining Dollars\n  Meal Swipes\n  Settings");
-            string userInput = Console.ReadLine();
-
+            Console.WriteLine("Type one of the following: \n  Dining Dollars\n  Meal Swipes\n  Info");
+            string userInput = Console.ReadLine().ToLower();
 
             int currentDayOfYear = DateTime.Today.DayOfYear;
-            int daysRemaining = SEMESTEREND - currentDayOfYear;
+            int daysRemaining = SEMESTER_END - currentDayOfYear;
             double weeksRemaining = daysRemaining/7.0;
-            int schoolDaysRemaining = (int)weeksRemaining*5;
+            int schoolDaysRemaining = (int)weeksRemaining*5; // Currently causes issue: integer will truncate decimal on weeksRemaining...  
 
-            // DINING DOLLARS
+            if(currentDayOfYear >= SEMESTER_END) {
+                Console.WriteLine("\nError, today's date is after the end of the semester. Values will be inaccurate. \nPress enter to continue anyway...");
+                Console.ReadLine();
+            }
+
+            // PARSE DINING DOLLARS
+            // specifically checks if input includes "di" so that a typo would still work. It shouldn't mess up our other options
             if(userInput.Contains("di") || userInput.Equals("d")) {
                 Console.WriteLine("How many dining dollars do you currently have?");
                 double currentDollars = double.Parse(Console.ReadLine());
 
                 // calculate: amount you can spend per week 
-                //            amount you can spend per day 
+                //            amount you can spend per day (but only show if it is applicable)
 
                 double amtPerWeek = currentDollars/weeksRemaining;
                 double amtPerDay = currentDollars/schoolDaysRemaining;
 
-                Console.WriteLine("You can spend " + amtPerWeek + " per week");
+                Console.WriteLine("You can spend " + Math.Round(amtPerWeek, 2) + " per week");
                 // we aren't going to show this message if they don't have more than $4 per day anyways
                 if(amtPerDay > 4)
-                    Console.WriteLine("You can spend " + amtPerDay + " per weekday");
+                    Console.WriteLine("You can spend " + Math.Round(amtPerDay, 2) + " per weekday");
             }
-            // SWIPES
+            // PARSE SWIPES
             else if(userInput.Contains("sw") || userInput.Equals("s")) {
                 Console.WriteLine("How many swipes do you currently have?");
                 int currentSwipes = int.Parse(Console.ReadLine());
 
-                Console.WriteLine("You can use " + currentSwipes/weeksRemaining + " swipes per week.");
-
-
+                Console.WriteLine("You can use " + Math.Round(currentSwipes/weeksRemaining, 1) + " swipes per week.");
             }
-            // SETTINGS
-            else if(userInput.Contains("set") || userInput.Equals("se")) {
-                Console.WriteLine("Entering settings...");
+            // INFO
+            else if(userInput.Contains("inf") || userInput.Equals("i")) {
+                Console.WriteLine("Displaying info...");
                 Console.WriteLine("  Today is the " + currentDayOfYear + " day of the year.");
                 Console.WriteLine("  There are " + daysRemaining + " days remaining in the semester.");
-                Console.WriteLine("  There are " + daysRemaining/7.0 + " weeks remaining in the semester.");
+                Console.WriteLine("  There are " + Math.Round(daysRemaining/7.0, 1) + " weeks remaining in the semester.");
                 Console.WriteLine("  There are " + schoolDaysRemaining + " week days remaining in the semester.");
-
             }
             else {
                 Console.WriteLine("Could not interpret " + userInput + " as a valid choice. \nPlease use \"dining dollars\", \"swipes\", or \"settings\".");
             }
             
-
-
-
         }
     }
 }
